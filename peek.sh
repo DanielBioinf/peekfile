ec28c37806ef6158ea6a2499c74f252bd9ae1145
Daniel Pérez <danielperezartiles@gmail.com>
2023-11-02 09:25:18 +0100

    3.0

--- peek.sh
+++ peek.sh
@@ -1,3 +1,23 @@
-head -n $2 $1
-echo "..."
-tail -n $2 $1
+num=$(cat $1 | wc -l)
+if [[ -n $2 ]]
+    then
+        if [[ $num -le 2$2 ]]   # I am not sure about the task being asked here(2x)
+            then 
+                cat $1
+            else
+                echo "Warning"
+                head -n $2 $1
+                echo "..."
+                tail -n $2 $1
+        fi
+    else
+        if [[ $num -le 23 ]]
+            then 
+                cat $1
+            else
+                echo "Warning"
+                head -n 3 $1
+                echo "..."
+                tail -n 3 $1
+        fi
+fi
